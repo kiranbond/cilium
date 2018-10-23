kiranbond/cilium

commit b0aeb2888ac658185ef9c608002a367a61b026a6

policy/api: add String function for Rules type
This allows for logging the the content of the rules. Previously, when rules
were imported, the logs would log the memory addresses of the rules in the Rules
object, which are useless for debugging. Now, one can see what the content of
the rules being added is in the logs if they so desire.

Also change the listing of rules in the logs to Info level.

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/policy.go
@@ -165,7 +165,7 @@ type AddOptions struct {
 // pods which are selected. Eventual changes in policy rules are propagated to
 // all locally managed endpoints.
 func (d *Daemon) PolicyAdd(rules policyAPI.Rules, opts *AddOptions) (uint64, error) {
-	log.WithField(logfields.CiliumNetworkPolicy, logfields.Repr(rules)).Debug("Policy Add Request")
+	log.WithField(logfields.CiliumNetworkPolicy, rules.String()).Info("Policy Add Request")
 
 	// These must be marked before actually adding them to the repository since a
 	// copy may be made and we won't be able to add the ToFQDN tracking labels

pkg/policy/api/rules.go
@@ -14,9 +14,44 @@
 
 package api
 
+import "fmt"
+
 // Rules is a collection of api.Rule.
 //
 // All rules must be evaluated in order to come to a conclusion. While
 // it is sufficient to have a single fromEndpoints rule match, none of
 // the fromRequires may be violated at the same time.
 type Rules []*Rule
+
+func (r Rules) String() string {
+
+	if len(r) == 0 {
+		return ""
+	}
+
+	if len(r) == 1 {
+		if r[0] != nil {
+			return fmt.Sprintf("%v", *r[0])
+		}
+		return "<nil>"
+	}
+
+	rulesString := ""
+
+	if r[0] != nil {
+		rulesString = fmt.Sprintf("[%v", *r[0])
+	} else {
+		rulesString = "[<nil>"
+	}
+	for i := 1; i < len(r); i++ {
+		if r[i] == nil {
+			rulesString += ", <nil>"
+		} else {
+			rulesString += fmt.Sprintf(", %v", *r[i])
+		}
+	}
+
+	rulesString += "]"
+
+	return rulesString
+}

commit 872f900120ed3ec19716f9c0e8424ae9a3064f05

daemon: change policyAdd message type from Info to Debug
Also, change the way we print the rules by also printing all fields
names of the policy being imported.

Fixes: b0aeb2888ac6 ("policy/api: add String function for Rules type")
Signed-off-by: André Martins <andre@cilium.io>

daemon/policy.go
@@ -169,7 +169,7 @@ type AddOptions struct {
 // pods which are selected. Eventual changes in policy rules are propagated to
 // all locally managed endpoints.
 func (d *Daemon) PolicyAdd(rules policyAPI.Rules, opts *AddOptions) (uint64, error) {
-	log.WithField(logfields.CiliumNetworkPolicy, rules.String()).Info("Policy Add Request")
+	log.WithField(logfields.CiliumNetworkPolicy, rules.String()).Debug("Policy Add Request")
 
 	// These must be marked before actually adding them to the repository since a
 	// copy may be made and we won't be able to add the ToFQDN tracking labels

pkg/policy/api/rules.go
@@ -14,7 +14,10 @@
 
 package api
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Rules is a collection of api.Rule.
 //
@@ -23,35 +26,12 @@ import "fmt"
 // the fromRequires may be violated at the same time.
 type Rules []*Rule
 
-func (r Rules) String() string {
+func (rs Rules) String() string {
+	strRules := make([]string, 0, len(rs))
 
-	if len(r) == 0 {
-		return ""
+	for _, r := range rs {
+		strRules = append(strRules, fmt.Sprintf("%+v", r))
 	}
 
-	if len(r) == 1 {
-		if r[0] != nil {
-			return fmt.Sprintf("%v", *r[0])
-		}
-		return "<nil>"
-	}
-
-	rulesString := ""
-
-	if r[0] != nil {
-		rulesString = fmt.Sprintf("[%v", *r[0])
-	} else {
-		rulesString = "[<nil>"
-	}
-	for i := 1; i < len(r); i++ {
-		if r[i] == nil {
-			rulesString += ", <nil>"
-		} else {
-			rulesString += fmt.Sprintf(", %v", *r[i])
-		}
-	}
-
-	rulesString += "]"
-
-	return rulesString
+	return "[" + strings.Join(strRules, ",") + "]"
 }